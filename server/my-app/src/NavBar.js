25c0c6a2d57bcde9533dae710ac294c2c5088d34
NeoInfy <pradeepkumar.ingle@infosys.com>
2022-01-13 18:24:32 +0530

    Project added

---
+++ server/my-app/src/NavBar.js
@@ -0,0 +1,20 @@
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Posts</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Create a Post</a></li>
+    </ul>
+  </div>
+</nav>
+    </div>
+  );
+}
+
+export default NavBar;
